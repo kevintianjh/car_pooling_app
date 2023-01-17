59e4f2300fa29d9ca2d65d51e56df3e409ebbc9a
eugenelye <eugenelye91@gmail.com>
2023-01-17 18:33:16 +0800

    tripController, tripRepo, tripService updated

---
+++ src/main/java/com/accenture/carpooling/controller/TripController.java
@@ -0,0 +1,62 @@
+package com.accenture.carpooling.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accenture.carpooling.entity.Customer;
+import com.accenture.carpooling.entity.Trip;
+import com.accenture.carpooling.service.TripService;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class TripController {
+	
+	private TripService tripService;
+	
+	public TripController(TripService tripService) {
+		super();
+		this.tripService = tripService;
+	}
+	
+	@PostMapping("/addTrip")
+	public ResponseEntity<Trip> saveTrip(@RequestBody Trip trip) {
+		return new ResponseEntity<Trip>(tripService.saveTrip(trip), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/getTrips")
+	public List<Trip> getAllTrips() {
+		return tripService.getAllTrips();
+	}
+	
+	@GetMapping("/getTripsByPage/{id}/{pageNo}")
+	public Page<Trip> getTripsByCustomerId(@PathVariable("pageNo") int pageNo, @PathVariable("id") Integer customerId) {
+		return tripService.getTripsByCustomerId(pageNo, customerId);
+	}
+	
+	@PutMapping("/updateTrip/{id}")
+	public ResponseEntity<Trip> updateTrip(@PathVariable("id") Integer tripId, @RequestBody Trip trip){
+		return new ResponseEntity<Trip>(tripService.updateTrip(trip, tripId), HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping({"/deleteTrip/{id}"})
+	public ResponseEntity<String> deleteTrip(@PathVariable("id") Integer id) {
+		tripService.deleteTrip(id);
+		return new ResponseEntity<String>("Trip Deleted Successfully", HttpStatus.OK);
+		
+	}
+	
+
+}
---
+++ src/main/java/com/accenture/carpooling/repository/TripRepository.java
@@ -0,0 +1,23 @@
+package com.accenture.carpooling.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+import com.accenture.carpooling.entity.Trip;
+
+
+public interface TripRepository extends JpaRepository<Trip, Integer>{
+
+	@Query("select t from Trip t where t.customer.id=?1")
+	public Page <Trip> getTripsByCustomerId (Pageable pageable, Integer customerId);
+		
+//	Page<Trip> findByPostalFromContaining(@Param("from_postal") String fromPostal, Pageable pageable );
+	
+}
