0b7bf20f26d967e6fb6107e6e65fbeadfcf367b3
kevintianjh <tianjhenhaokevin@gmail.com>
2023-01-18 19:26:33 +0800

    kevin
    kevin

--- src/main/java/com/accenture/carpooling/controller/TripController.java
+++ src/main/java/com/accenture/carpooling/controller/TripController.java
@@ -1,32 +1,24 @@
 package com.accenture.carpooling.controller;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
+import java.util.List; 
+import org.springframework.http.HttpStatus; 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;HEAD
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.CrossOrigin; 
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.accenture.carpooling.entity.Customer;
+import org.springframework.web.bind.annotation.RestController; 
 import com.accenture.carpooling.entity.Trip;
-import com.accenture.carpooling.service.TripService;
+import com.accenture.carpooling.json.JsonResponseBase;
+import com.accenture.carpooling.service.TripService; 
+import jakarta.servlet.http.HttpServletRequest;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/customer")
-public class TripController {
-	
+public class TripController { 
 	private TripService tripService;
 	
 	public TripController(TripService tripService) {
@@ -38,30 +30,51 @@ public class TripController {
 	public ResponseEntity<Trip> saveTrip(@RequestParam("header_id") Integer customerId, @RequestBody Trip trip) {
 		return new ResponseEntity<Trip>(tripService.saveTrip(customerId, trip), HttpStatus.CREATED);
 	}
-	
-	@GetMapping("/getTrips")
-	public List<Trip> getAllTrips() {
-		return tripService.getAllTrips();
-	}
-	
 
 	@GetMapping("/trip/list")
 	public List<Trip> getTripsByCustomerId(@RequestParam("header_id") Integer customerId) {
 		return tripService.getTripsByCustomerId(customerId);
 	} 
 	
-	@PutMapping("/updateTrip/{id}")
-	public ResponseEntity<Trip> updateTrip(@PathVariable("id") Integer tripId, @RequestBody Trip trip){
-		return new ResponseEntity<Trip>(tripService.updateTrip(trip, tripId), HttpStatus.OK);
+	@PostMapping("/trip/update")
+	public JsonResponseBase updateTrip(HttpServletRequest request) {
+		Integer customerId = Integer.parseInt(request.getParameter("header_id"));
+		Integer tripId = Integer.parseInt(request.getParameter("id"));
 		
+		Trip retrievedTrip = this.tripService.findById(tripId);
+		
+		if(retrievedTrip.getCustomer().getId() != customerId) {
+			throw new RuntimeException("Object don't belong to customer!");
+		}
+		
+		retrievedTrip.setFromPostal(request.getParameter("fromPostal"));
+		retrievedTrip.setToPostal(request.getParameter("toPostal"));
+		retrievedTrip.setDays(request.getParameter("days"));
+		retrievedTrip.setDescription(request.getParameter("description"));
+		retrievedTrip.setRole(Integer.parseInt(request.getParameter("role")));
+		retrievedTrip.setTimeOfDay(Integer.parseInt(request.getParameter("timeOfDay"))); 
+		
+		this.tripService.save(retrievedTrip); 
+		JsonResponseBase rsp = new JsonResponseBase();
+		rsp.header_rsp = "ok";
+		
+		return rsp;
 	}
 	
-	@DeleteMapping({"/deleteTrip/{id}"})
-	public ResponseEntity<String> deleteTrip(@PathVariable("id") Integer id) {
-		tripService.deleteTrip(id);
-		return new ResponseEntity<String>("Trip Deleted Successfully", HttpStatus.OK);
+	@PostMapping("/trip/delete")
+	public JsonResponseBase deleteTrip(HttpServletRequest request) {
+		Integer customerId = Integer.parseInt(request.getParameter("header_id"));
+		Integer tripId = Integer.parseInt(request.getParameter("id"));
 		
-	}
-	
-
+		Trip retrievedTrip = this.tripService.findById(tripId);
+		
+		if(retrievedTrip.getCustomer().getId() != customerId) {
+			throw new RuntimeException("Object don't belong to customer!");
+		}
+		
+		this.tripService.deleteTrip(retrievedTrip);
+		JsonResponseBase rsp = new JsonResponseBase();
+		rsp.header_rsp = "ok";
+		return rsp;
+	} 
 }
--- src/main/java/com/accenture/carpooling/service/TripService.java
+++ src/main/java/com/accenture/carpooling/service/TripService.java
@@ -35,10 +35,6 @@ public class TripService {
 		return tripRepository.save(trip);
 	};
 	
-	public List<Trip> getAllTrips() {
-		return tripRepository.findAll();
-	};
-	
 	public Trip getTripbyId(Integer customerId) {
 		return tripRepository.findById(customerId).orElseThrow(()-> new ResourceNotFoundException("Trip", "Id", customerId));
 	};
@@ -56,16 +52,17 @@ public class TripService {
 		tripRepository.save(existingTrip);
 		return existingTrip;
 		
+	}; 
+	
+	public void deleteTrip(Trip trip) {
+		this.tripRepository.delete(trip);
 	};
 	
-	
-	public void deleteTrip(Integer id) {
-		//check whether the employee exist in database.
-		Trip trip = tripRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee", "Id", id));
-		
-		tripRepository.delete(trip);
-	};
-	
-
-
+	public Trip findById(Integer id) {
+		return this.tripRepository.findById(id).get();
+	}
+	 
+	public void save(Trip trip) {
+		this.tripRepository.save(trip);
+	}
 }
