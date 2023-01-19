d21f2ebf0a6167d981ea7c570376d5aefe94ab74
kevintianjh <tianjhenhaokevin@gmail.com>
2023-01-19 15:22:31 +0800

    k
    k

--- src/main/java/com/accenture/carpooling/controller/TripController.java
+++ src/main/java/com/accenture/carpooling/controller/TripController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.web.bind.annotation.CrossOrigin; 
 import org.springframework.web.bind.annotation.GetMapping;  
-import org.springframework.web.bind.annotation.PostMapping;   
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController; 
 import com.accenture.carpooling.entity.Customer;
@@ -18,7 +19,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @CrossOrigin
 @RestController
-//@RequestMapping("/customer")
+@RequestMapping("/customer")
 public class TripController { 
 	
 	@Autowired
