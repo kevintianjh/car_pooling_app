35bd8ce88121fe410355d81d5d337def27ee0e17
kevintianjh <tianjhenhaokevin@gmail.com>
2023-01-19 15:59:10 +0800

    k
    k

--- src/main/java/com/accenture/carpooling/controller/AuthenticationFilter.java
+++ src/main/java/com/accenture/carpooling/controller/AuthenticationFilter.java
@@ -16,9 +16,9 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@CrossOrigin
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//@CrossOrigin
 public class AuthenticationFilter implements Filter {  
 	
 	private ObjectMapper objectMapper = new ObjectMapper();
--- src/main/java/com/accenture/carpooling/repository/TripRepository.java
+++ src/main/java/com/accenture/carpooling/repository/TripRepository.java
@@ -14,4 +14,4 @@ public interface TripRepository extends JpaRepository<Trip, Integer>{
 	
 	@Query("select t from Trip t WHERE SUBSTRING(t.fromPostal, 1, 3)=?1 AND SUBSTRING(t.toPostal, 1, 3)=?2 AND t.days LIKE %?3% AND t.timeOfDay LIKE %?4%" )
 	List<Trip> getTripsWithSameDestination(String fromPostal,String toPostal, String day, String timeOfDay);
- }
+}
